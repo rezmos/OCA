c9f3a0354acc9d6a2a8bcd4a594e8f4bf0c4e04a
Alejandro Arevalo <alejandro.arevalo@wnco.com>
2018-01-17 17:36:43 -0500

    Initial commit

---
+++ src/main/java/com/globant/oca/chapter1/Clazz.java
@@ -0,0 +1,8 @@
+package com.globant.oca.chapter1;
+
+public class Clazz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello... Welcome to the OCA training!");
+    }
+}
---
+++ src/test/java/com/globant/oca/chapter1/ClazzTest.java
@@ -0,0 +1,8 @@
+package com.globant.oca.chapter1;
+
+public class ClazzTest {
+
+    public void test(){
+        System.out.println("This is a test");
+    }
+}
